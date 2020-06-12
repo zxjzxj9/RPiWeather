26c697ad48c800e0b7cca8c09e8ed650a9d763a7
Victor Zhang <zxjzxj9@gmail.com>
2020-06-12 23:39:32 +0800

    Add table

--- backend/init_tbl.sql
+++ backend/init_tbl.sql
@@ -1,8 +1,14 @@
 drop table if exists weather_param;
+drop table if exists request_record;
 create table weather_param (
-    record_id serial PRIMARY KEY,
+    record_id serial primary key,
     temperature real,
     humidity real,
     pressure real,
     record_time timestamp
+);
+create table request_record (
+    request_id serial primary key,
+    request_time timestamp,
+    record_id int references weather_param(record_id)
 );
