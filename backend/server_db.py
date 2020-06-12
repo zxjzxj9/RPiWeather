45d39c0faa44f841ea3b52caebd3d8de82619e2b
Victor Zhang <zxjzxj9@gmail.com>
2020-06-13 02:18:12 +0800

    Add some server db logics

--- backend/server_db.py
+++ backend/server_db.py
@@ -12,12 +12,17 @@ import sqlalchemy
 # from sqlalchemy import Column, Integer, String
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Table, MetaData, create_engine
+from sqlalchemy import Table, MetaData, create_engine, and_, or_
 from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.ext.automap import automap_base
 
 from weather_restful import app
 
-engine = create_engine("postgresql:///weather", echo=True)
+engine = create_engine("postgresql:///weather")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+metadata = MetaData()
+metadata.reflect(bind=engine)
 Session = sessionmaker(bind=engine)
 
 @app.before_request
@@ -29,6 +34,27 @@ def shutdown_session(response_or_exc):
     flask.g.session.commit()
     flask.g.session.remove()
 
+class TableList:
+    def __init__(self):
+        pass
+
+    def get_table(self, table_name):
+        return getattr(Base.classes, table_name)
+
+    def __getitem__(self, table_name):
+        return self.get_table(table_name)
+
+    @property
+    def names(self):
+        return list(metadata.tables.keys())
 
 if __name__ == "__main__":
-    pass
+    tl = TableList()
+    print(tl.names)
+    print(tl["weather_param"])
+    import datetime
+    wp_table = tl["weather_param"]
+    cmd = Session().query(wp_table).filter(and_(wp_table.record_time > datetime.datetime.now() - datetime.timedelta(hours=2),
+                                           wp_table.record_time < datetime.datetime.now() - datetime.timedelta(hours=1)))
+    for rec in cmd.all():
+        print(rec.temperature, rec.record_time)
