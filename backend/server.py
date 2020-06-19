9f6739abc66a097b2befc2b62056dbe9ff5def53
Victor Zhang <zxjzxj9@gmail.com>
2020-06-19 23:56:00 +0800

    Add OpenAPI3 support

--- backend/server.py
+++ backend/server.py
@@ -8,21 +8,12 @@ from flask import Flask, request
 from weather_restful import app
 import argparse
 
+
 parser = argparse.ArgumentParser(description='Flask data RESTful server')
 parser.add_argument("-b", "--host", type=str, default="0.0.0.0", help="Binding host")
 parser.add_argument("-p", "--port", type=int, default=8080, help="Binding port")
 parser.add_argument("-d", "--debug", action="store_true", help="Debug mode")
 args = parser.parse_args()
-
-
-@app.route("/")
-def example():
-    return {"status": "success"}, 200
-
-@app.route("/api/date_range")
-def date_ramge():
-    pass
-
 
 if __name__ == "__main__":
     app.run(host=args.host, port=args.port, debug=args.debug)
--- backend/weather_restful.py
+++ backend/weather_restful.py
@@ -1,4 +1,12 @@
 #! /usr/bin/env python
 
 from flask import Flask
+from flasgger import Swagger
+from flask_restful import Api, Resource
+from resources import Example
+
 app = Flask(__name__)
+api = Api(app)
+swagger = Swagger(app)
+
+api.add_resource(Example, '/example')
