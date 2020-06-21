28def925dcd460f404040b8f1b820981d9d2fdaf
Victor Zhang <zxjzxj9@gmail.com>
2020-06-21 15:35:07 +0800

    Add some frontend code

---
+++ frontend/weather_forecast/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {Rating} from 'semantic-ui-react';
+// import d3 from 'd3';
+
+function App() {
+  return (
+      <Rating rating={1} maxRating={5} />
+
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+  );
+}
+
+export default App;
