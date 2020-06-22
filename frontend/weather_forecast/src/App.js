bad29a6a63013f8d69966599ecaa3a2baecbbdca
Victor Zhang <zxjzxj9@gmail.com>
2020-06-22 22:28:46 +0800

    Add side bar (to be modified

--- frontend/weather_forecast/src/App.js
+++ frontend/weather_forecast/src/App.js
@@ -1,29 +1,49 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import {Rating} from 'semantic-ui-react';
+//import {Rating} from 'semantic-ui-react';
 // import d3 from 'd3';
+import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
+const SidebarExampleVisible = () => (
+  <Sidebar.Pushable as={Segment}>
+    <Sidebar
+      as={Menu}
+      animation='push'
+      icon='labeled'
+      inverted
+      vertical
+      visible
+      width='thin'
+    >
+      <Menu.Item as='a'>
+        <Icon name='home' />
+        Home
+      </Menu.Item>
+      <Menu.Item as='a'>
+        <Icon name='gamepad' />
+        Games
+      </Menu.Item>
+      <Menu.Item as='a'>
+        <Icon name='camera' />
+        Channels
+      </Menu.Item>
+    </Sidebar>
+
+    <Sidebar.Pusher>
+      <Segment basic>
+        <Header as='h3'>Application Content</Header>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Segment>
+    </Sidebar.Pusher>
+  </Sidebar.Pushable>
+)
+
+
+// color scheme: https://coolors.co/ffa69e-faf3dd-b8f2e6-aed9e0-5e6472
 function App() {
   return (
-      <Rating rating={1} maxRating={5} />
-
-//    <div className="App">
-//      <header className="App-header">
-//        <img src={logo} className="App-logo" alt="logo" />
-//        <p>
-//          Edit <code>src/App.js</code> and save to reload.
-//        </p>
-//        <a
-//          className="App-link"
-//          href="https://reactjs.org"
-//          target="_blank"
-//          rel="noopener noreferrer"
-//        >
-//          Learn React
-//        </a>
-//      </header>
-//    </div>
+    <SidebarExampleVisible />
   );
 }
 
