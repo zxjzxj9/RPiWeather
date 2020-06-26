b61fa0f08508b947560f108e0431578ae0c8c206
Victor Zhang <zxjzxj9@gmail.com>
2020-06-26 22:37:08 +0800

    Add react.js behavior

--- frontend/weather_forecast/src/Sidebar.js
+++ frontend/weather_forecast/src/Sidebar.js
@@ -10,12 +10,33 @@ class Index extends React.Component {
   }
 }
 
+class Chart extends React.Component {
+
+  render() {
+    return (
+      <Header as='h3'>Weather Data Chart</Header>
+    );
+  }
+}
+
 class MainPage extends React.Component {
     
   constructor(props) {
     super(props);
-      this.content = <Index />;
+    this.state = {content: 'index'};
+  }
+
+  handleClick(componentName) {
+    this.setState({content: componentName});
+  }
+
+  renderSideBar() {
+    switch(this.state.content) {
+      case 'index': return <Index />;
+      case 'chart': return <Chart />;
+      default: return <Index />;
     }
+  }
 
   render() {
     return (
@@ -28,20 +49,20 @@ class MainPage extends React.Component {
           vertical
           visible
           width='thin'
-        >
-        <Menu.Item as='a' onClick={function() {alert('Click!');}}>
+      >
+        <Menu.Item as='a' onClick={this.handleClick.bind(this, "chart")}>
           <Icon name='chart line' />
           Weather Data
         </Menu.Item>
-        <Menu.Item as='a'>
+        <Menu.Item as='a' onClick={this.handleClick.bind(this, "ml")}>
           <Icon name='microchip' />
           Machine Learning Pipeline
         </Menu.Item>
-        <Menu.Item as='a'>
+        <Menu.Item as='a' onClick={this.handleClick.bind(this, "settings")}>
           <Icon name='settings' />
           Settings
         </Menu.Item>
-        <Menu.Item as='a'>
+        <Menu.Item as='a' onClick={this.handleClick.bind(this, "info")}>
           <Icon name='info' />
           Info
         </Menu.Item>
@@ -49,7 +70,7 @@ class MainPage extends React.Component {
 
       <Sidebar.Pusher style={{"min-height": "100vh"}}>
         <Segment basic>
-	    {this.content}
+	    {this.renderSideBar()}
         </Segment>
       </Sidebar.Pusher>
     </Sidebar.Pushable>
