83c18492d0f1ddbd866f64e7ec29b05de0f7310c
Victor Zhang <zxjzxj9@gmail.com>
2020-07-06 23:11:06 +0800

    Need to have a change in promise code

--- frontend/weather_forecast/src/Chart.tsx
+++ frontend/weather_forecast/src/Chart.tsx
@@ -30,20 +30,39 @@ class ChartImg extends React.Component<any, any>{
   }
 
   fetchDraw() {
-    const data = this.fetchData();
-    console.log(data);
+    //const data = this.fetchData();
+    //console.log(data);
     const curr = d3.select(this.ref.current);
-    curr.append("circle")
-          .attr("cx", 40)
-          .attr("cy", 60)
-          .attr("r", 10);
+    curr.selectAll("svg > *").remove();
+
+    // From https://datawanderings.com/2019/10/28/tutorial-making-a-line-chart-in-d3-js-v-5/
+    const xScale = d3.scaleTime().range([0,this.ref.current.style.width]);
+    const yScale = d3.scaleLinear().rangeRound([this.ref.current.style.height, 0]);
+
+    //const ts = data!["timestamp"];
+    //console.log(ts);
+    //xScale.domain(d3.extent(ts.map(item:string => new Date(item))));
+    //xScale.domain(d3.extent(data["timestamp"].map function(d){
+    //    return new Date(d);}));
+
+    // curr.append("circle")
+    //      .attr("cx", Math.random()*30)
+    //      .attr("cy", Math.random()*30)
+    //      .attr("r", Math.random()*10);
+  }
+
+  componentDidMount() {
+     this.fetchDraw();
+  }
+
+  componentDidUpdate() {
+     this.fetchDraw();
   }
 
   render() {
     return (
         <div>
           <svg style={{height:400, width:600}} ref={this.ref}/>
-          {this.fetchDraw()}
         </div>
     );
   }
