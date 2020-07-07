8ae0c39f798ab2f4517cadb0b384fb6b6532aec1
Victor Zhang <zxjzxj9@gmail.com>
2020-07-07 19:37:05 +0800

    Still some issues with  d3 sacler

--- frontend/weather_forecast/src/Chart.tsx
+++ frontend/weather_forecast/src/Chart.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import * as d3 from 'd3';
+import { tsv } from 'd3';
 
 
-class ChartImg extends React.Component<any, any>{
+interface WeatherData {
+    humidity: number[];
+    pressure: number[];
+    temperature: number[];
+    timestamp: string[];
+}
+
+class ChartImg extends React.Component<any, any> {
 
   private ref: any;
   constructor(props: any) {
@@ -11,33 +19,47 @@ class ChartImg extends React.Component<any, any>{
     this.ref = React.createRef();
   }
 
-  fetchData(){
+  fetchDraw(){
     var host = "http://192.168.199.249:8080";
     var ret;
-    axios.post(host + "/date_weather", {
-      'start': this.props.start.toISOString(),
-      'end': this.props.end.toISOString()
-    }).then(function (response) {
-      const data = response.data;
-      console.log(data);
-      //return data;
-      ret = data;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    return ret;
+    const thisref = this;
+    return axios.post<WeatherData>(host + "/date_weather", {
+             'start': this.props.start.toISOString(),
+             'end': this.props.end.toISOString()
+           }).then(function (response) {
+             const data = response.data;
+             // console.log(data);
+             thisref.drawData(data);
+             return data;
+           })
+             .catch(function (error) {
+             console.log(error);
+           });
   }
 
-  fetchDraw() {
+  drawData(data: WeatherData) {
     //const data = this.fetchData();
     //console.log(data);
     const curr = d3.select(this.ref.current);
     curr.selectAll("svg > *").remove();
 
     // From https://datawanderings.com/2019/10/28/tutorial-making-a-line-chart-in-d3-js-v-5/
-    const xScale = d3.scaleTime().range([0,this.ref.current.style.width]);
+    const xScale = d3.scaleTime().range([this.ref.current.style.width, 0]);
     const yScale = d3.scaleLinear().rangeRound([this.ref.current.style.height, 0]);
+
+    // xScale.domain(data.timestamp.map(item => new Date(item)));
+    const datetime = data.timestamp.map(item => new Date(item))
+    const maxmin = d3.extent(datetime);
+
+    //xScale.range([0, 100]);
+    xScale.domain(maxmin as [Date, Date]);
+    //xScale.domain([Date.now(), Date.now() + 24 * 60 * 60 * 1000]);
+    console.log(maxmin);
+
+    const xAxis = d3.axisBottom(xScale);
+
+    curr.append("g")
+          .call(xAxis);
 
     //const ts = data!["timestamp"];
     //console.log(ts);
