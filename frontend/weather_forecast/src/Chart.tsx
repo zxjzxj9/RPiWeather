26fe6fd8b207b7ae91851059c45eb92702439604
Victor Zhang <zxjzxj9@gmail.com>
2020-07-10 21:47:15 +0800

    Add basic axis

--- frontend/weather_forecast/src/Chart.tsx
+++ frontend/weather_forecast/src/Chart.tsx
@@ -43,24 +43,99 @@ class ChartImg extends React.Component<any, any> {
     const curr = d3.select(this.ref.current);
     curr.selectAll("svg > *").remove();
 
+    const margin = {
+      top: 50,
+      bottom: 50,
+      left: 50,
+      right: 50
+    }
+
+    const height = parseInt(this.ref.current.style.height) - (margin.top + margin.bottom);
+    const width = parseInt(this.ref.current.style.width) - (margin.left + margin.right);
     // From https://datawanderings.com/2019/10/28/tutorial-making-a-line-chart-in-d3-js-v-5/
-    const xScale = d3.scaleTime().range([this.ref.current.style.width, 0]);
-    const yScale = d3.scaleLinear().rangeRound([this.ref.current.style.height, 0]);
+    // console.log(this.ref.current.style.width, this.ref.current.style.height)
+    const xScale = d3.scaleTime().range([0, width]);
+    const yScale_T = d3.scaleLinear().rangeRound([height, 0]);
+    const yScale_P = d3.scaleLinear().rangeRound([height, 0]);
+    const yScale_H = d3.scaleLinear().rangeRound([height, 0]);
+
+    yScale_T.domain(d3.extent(data.temperature) as [number, number]);
+    yScale_P.domain(d3.extent(data.pressure) as [number, number]);
+    yScale_H.domain(d3.extent(data.humidity) as [number, number]);
 
     // xScale.domain(data.timestamp.map(item => new Date(item)));
     const datetime = data.timestamp.map(item => new Date(item))
-    const maxmin = d3.extent(datetime);
-
-    //xScale.range([0, 100]);
-    xScale.domain(maxmin as [Date, Date]);
+    xScale.domain(d3.extent(datetime) as [Date, Date]);
     //xScale.domain([Date.now(), Date.now() + 24 * 60 * 60 * 1000]);
-    console.log(maxmin);
+    //console.log(maxmin);
 
     const xAxis = d3.axisBottom(xScale);
+    const yAxisT = d3.axisLeft(yScale_T);
+    const yAxisP = d3.axisLeft(yScale_P);
+    const yAxisH = d3.axisRight(yScale_H);
+
+    function trans(x: number, y:number) {
+      return "translate(" + (x + margin.left) + ", " + (y + margin.top) + ")";
+    }
 
     curr.append("g")
-          .call(xAxis);
+          .attr("transform", trans(0, height))
+          .call(xAxis)
+          .append("text")
+          .attr("class", "axis-title")
+          .attr("transform", "rotate(0)")
+          .attr("x", width/2)
+          .attr("y", 20)
+          .attr("dy", "2.00em")
+          .style("text-anchor", "end")
+          .attr("fill", "#000000")
+          .text("Datetime");
+    
+    curr.append("g")
+          .attr("transform", trans(0, 0))
+          .call(yAxisT)
+          .append("text")
+          .attr("class", "axis-title")
+          .attr("transform", "rotate(-90)")
+          .attr("x", 0)
+          .attr("y", -60)
+          .attr("dy", "2.00em")
+          .style("text-anchor", "end")
+          .attr("fill", "#000000")
+          .text("Temperature/°C");
+    
+    curr.append("g")
+          .attr("transform", trans(width, 0))
+          .call(yAxisP)
+          .append("text")
+          .attr("class", "axis-title")
+          .attr("x", 50)
+          .attr("y", 0)
+          .attr("transform", "rotate(-90)")
+          .attr("dy", "2.00em")
+          .style("text-anchor", "end")
+          .attr("fill", "#000000")
+          .text("Pressure/hBar");
 
+    curr.append("g")
+          .attr("transform", trans(width, 0))
+          .call(yAxisH)
+          .append("text")
+          .attr("class", "axis-title")
+          .attr("x", 50)
+          .attr("y", 0)
+          .attr("transform", "rotate(+90)")
+          .attr("dy", "2.00em")
+          .style("text-anchor", "end")
+          .attr("fill", "#000000")
+          .text("Humidity%");
+    //curr.append("g")
+    //      .attr("transform", "translate(" + width + ", 0)") 
+    //      .call(yAxisP);
+
+    //curr.append("g")
+    //      .attr("transform", "translate(" + width + ", 0)") 
+    //      .call(yAxisH);
     //const ts = data!["timestamp"];
     //console.log(ts);
     //xScale.domain(d3.extent(ts.map(item:string => new Date(item))));
@@ -84,7 +159,7 @@ class ChartImg extends React.Component<any, any> {
   render() {
     return (
         <div>
-          <svg style={{height:400, width:600}} ref={this.ref}/>
+          <svg style={{height:500, width:700}} ref={this.ref}/>
         </div>
     );
   }
