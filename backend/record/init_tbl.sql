7892fd606ab7e5eee2af6d0136d8e6648bd3b3e1
Victor Zhang <zxjzxj9@gmail.com>
2020-07-17 16:05:27 +0800

    Add weather request sql table

--- backend/record/init_tbl.sql
+++ backend/record/init_tbl.sql
@@ -12,3 +12,35 @@ create table request_record (
     request_time timestamp,
     record_id int references weather_param(record_id)
 );
+
+create table weather_request (
+    request_id serial primary key,
+    request_time timestamp,
+    coord_lon real,
+    coord_lat real,
+    weather_id real
+    weather_main text,
+    weather_description text,
+    weather_icon text,
+    base text,
+    main_temp real,
+    main_feels_like real,
+    main_temp_min real,
+    main_temp_max real,
+    main_pressure real,
+    main_humidity real,
+    visibility real,
+    wind_speed real,
+    wind_deg real,
+    clouds_all real,
+    dt int,
+    sys_type int,
+    sys_id int,
+    sys_country text,
+    sys_sunrise int,
+    sys_sunset int,
+    timezone int,
+    id int,
+    "name" int,
+    cod int
+);
