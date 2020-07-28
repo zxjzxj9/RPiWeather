45082ae111ecac101f2f18622b428bed3056d6af
Victor Zhang <zxjzxj9@gmail.com>
2020-07-28 20:09:59 +0800

    Modify the OpenWeatherDaemon code

--- backend/record/openweather_daemon.py
+++ backend/record/openweather_daemon.py
@@ -16,6 +16,22 @@ CITY="Shanghai"
 # See the apidoc in https://openweathermap.org/current
 URL="https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apikey}"
 
+SQL_CONN_STR = "postgresql:///weather"
+
+class OpenWeatherDaemon(Daemon):
+
+    def __init__(self, pidfile, interval):
+        super().__init__(pidfile)
+        self.interval = interval
+    
+    def run(self):
+        self.log.info("Weather collecting process started...")
+        engine = create_engine(SQL_CONN_STR)
+
+        while True:
+            # ret = insert_data(engine)
+            # self.log.info(str(ret))
+            time.sleep(self.interval)
 
 if __name__ == "__main__":
     pass
